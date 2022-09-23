074c050a92cf217195cddfdcf3b99b56084ab6a9
Mario Lugo <mlugo23699@gmail.com>
2022-09-23 10:44:44 -0700

    created class models for comment and reply's models.py files + generated serializers.py files for each app

--- backend/reply/models.py
+++ backend/reply/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from authentication.models import User
+from comment.models import Comment
 
 # Create your models here.
+
+class Reply(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment= models.ForeignKey(Comment, max_length=255)
+    text = models.CharField(max_length=255)
