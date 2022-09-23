e22b3f824478b7eacf17bab7cc2dc15230559799
Theo Gill <theogill1369@gmail.com>
2022-09-23 15:16:48 -0400

    Had to reclone down repo again, and re-add files for project

---
+++ backend/drf_jwt_backend/local_setting.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'youtube_clone_database',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'password',
+    }
+}
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-3ka=r4l61$y$f2y@77u#e@wndb%a4m+3s8d)g1wwgg=+^mlr&+'
