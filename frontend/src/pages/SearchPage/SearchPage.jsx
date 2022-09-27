22a9120a8d95cc64a9cb6d660e3de170d958de2b
unknown <bensandlin2@gmail.com>
2022-09-27 13:27:34 -0500

    Managed to get the website working again, only to find more problems.

--- frontend/src/pages/SearchPage/SearchPage.jsx
+++ frontend/src/pages/SearchPage/SearchPage.jsx
@@ -11,6 +11,19 @@ function SearchPage() {
     setSearchedVids(response.data.items)
   };
 
+  return (
+    <div>
+      <iframe
+        id="ytplayer"
+        type="text/html"
+        width="640"
+        height="360"
+        src={`https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com`}
+        frameBorder="0"
+      ></iframe>
+    </div>
+  );
+
 }
 
 export default SearchPage;
---
+++ frontend/src/pages/VideoPage/VideoPage.jsx
@@ -0,0 +1,7 @@
+
+
+function VideoPage() {
+    console.log("VideoPage being a butt")
+    return null;
+}
+export default VideoPage;
