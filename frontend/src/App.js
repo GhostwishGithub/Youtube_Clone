efc354e2b58d37cfec5381a4e01d49e4dce75872
unknown <bensandlin2@gmail.com>
2022-09-27 10:07:06 -0500

    Added in the routes and wrapped the appropiate routes in <PrivateRoute>. Setup step 2 is complete.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,9 @@ import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import SearchPage from "./pages/SearchPage/SearchPage";
+import VideoPage from "./pages/VideoPage/VideoPage";
+
 
 // Component Imports
 import Navbar from "./components/NavBar/NavBar";
@@ -24,11 +27,16 @@ function App() {
           element={
             <PrivateRoute>
               <HomePage />
+              <SearchPage />
+              <VideoPage />
             </PrivateRoute>
           }
         />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/video" element={<VideoPage />} />
       </Routes>
       <Footer />
     </div>
