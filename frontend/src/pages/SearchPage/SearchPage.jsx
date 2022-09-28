ea02c8f12f727b9fdf541d8ac4731c2ad1d0766b
Theo Gill <theogill1369@gmail.com>
2022-09-27 23:47:25 -0400

    Was able to get searchpage functionaility + related links

--- frontend/src/pages/SearchPage/SearchPage.jsx
+++ frontend/src/pages/SearchPage/SearchPage.jsx
@@ -13,20 +13,22 @@ const SearchPage = () => {
           console.log("RESPONSE COMING FROM SEARCHVIDEOS ",response)
           setSearchedVids(response.data.items);
         } catch (error) {
-          console.log(error.response.data);
+          console.log(error.response.data.items[0].id);
         }
       };
       fetchSearchVids();
     }, [token]);
     return (
       <div className="container">
-        {searchedVids &&
-          searchedVids.map((user) => (
-            <p key={searchedVids.id}>
-              {searchedVids.video_id} {searchedVids.name} {searchedVids.title}
-              { <img src ='https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com' alt="Demo Page"/> }
-            </p>
-          ))}
+            <iframe
+            id="ytplayer"
+            type="text/html"
+            width="640"
+            height="360"
+              key={searchedVids.id}
+              src ={'https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com'}
+              frameborder="0"
+            ></iframe>
       </div>
     );
   };
