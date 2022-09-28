96fa40cadf0387070867892389661c2d2b6ed593
unknown <bensandlin2@gmail.com>
2022-09-28 10:04:58 -0500

    Fixed the render not returning error.

--- frontend/src/pages/SearchPage/SearchPage.jsx
+++ frontend/src/pages/SearchPage/SearchPage.jsx
@@ -18,39 +18,19 @@ const SearchPage = () => {
       };
       fetchSearchVids();
     }, [token]);
-
-    const SearchButton = (props) => {
-
-      const [Search, setSearch] = useState("inactive");
-    
-      function SearchClick(){
-       if(Search === "inactive"){
-        setSearch("active");
-       }
-       else{
-        setSearch("inactive");
-       } 
-       props.SearchClick.map(Search)
-      }
+  
     return (
-      <div>
+      <div className="container">
             <iframe
             id="YouTube Clone"
             type="text/html"
             width="640"
             height="360"
-              key={searchedVids.id}
-              src ={'https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com'}
-              frameBorder="0"
+            key={searchedVids.id}
+            src ={'https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com'}
+            frameborder="0"
             ></iframe>
-            <div>
-              <input type="text" onClick={SearchPage} onChange={(selection)=>console.log(selection.target.value)} className="searchTerm" placeholder="SEARCH"></input>
-              <button type='submit' className="searchButton">
-              <i className="fa-fa search">search</i>
-              </button>
-            </div>
       </div>
     );
   };
-}
   export default SearchPage;
