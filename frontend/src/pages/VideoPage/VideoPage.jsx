44aad7b1cff6734ce5e4c1816f984034915af11a
Theo Gill <theogill1369@gmail.com>
2022-09-29 10:43:00 -0400

    Started setup of VideoPage

--- frontend/src/pages/VideoPage/VideoPage.jsx
+++ frontend/src/pages/VideoPage/VideoPage.jsx
@@ -1,7 +1,23 @@
+import DisplaySearchResults from "../SearchPage/DisplaySearchResults";
+import SearchPage from "../SearchPage/SearchPage";
 
 
-function VideoPage() {
-    console.log("VideoPage being a butt")
-//    return null;
-}
+
+const DisplaySearchResults = () => {
+return(
+<div>
+ <iframe
+ id="YouTube Clone"
+ type="text/html"
+ width="640"
+ height="360"
+ key={VideoPage}
+ src ={"https://i.ytimg.com/vi/_caMQpiwiaU/default.jpg"}
+ frameborder="0"
+ ></iframe>
+
+ <VideoPage SearchPage={SearchPage}/>
+ </div>
+)
+};
 export default VideoPage;
