31e492e559d0172688edce4d9e5ef85092a273ec
Theo Gill <theogill1369@gmail.com>
2022-09-29 14:04:32 -0400

    Attempt to make VideoPage responsive

--- frontend/src/pages/VideoPage/VideoPage.jsx
+++ frontend/src/pages/VideoPage/VideoPage.jsx
@@ -1,24 +1,38 @@
+import { useEffect, useState } from "react";
 import DisplaySearchResults from "../SearchPage/DisplaySearchResults";
 import SearchPage from "../SearchPage/SearchPage";
 
+const VideoPage = (props) => {
+    const [videoId, setVideoId] = useState([]);
+    
+    useEffect(() => {
+        
+    }, []);
+    
+        console.log("Video Page", videoId);
+    };
+    async function handleSubmit(event) {
+        event.preventDefault()
+        let SearchResult = {            
+            videoId: videoId,
+       
+        };       
+     
+    return (
+        <form onSubmit={handleSubmit}>
+            <iframe
+                id="YouTube Clone"
+                type="text/html"
+                width="640"
+                height="360"
+                key={VideoPage}
+                src ={"https://i.ytimg.com/vi/_caMQpiwiaU/default.jpg"}
+                frameborder="0"
+            ></iframe>
 
-
-const VideoPage = () => {
-return(
-<div>
- <iframe
- id="YouTube Clone"
- type="text/html"
- width="640"
- height="360"
- key={VideoPage}
- src ={"https://i.ytimg.com/vi/_caMQpiwiaU/default.jpg"}
- frameborder="0"
- ></iframe>
-
- {/* <VideoPage SearchPage={SearchPage}/> */}
- 
- </div>
-)
-};
+        {<VideoPage SearchPage={SearchPage}/> }
+        {<DisplaySearchResults SearchPage={SearchPage}/> }
+        </form>
+    );
+    } 
 export default VideoPage;
